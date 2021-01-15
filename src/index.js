21e5278de596f17b80a31b306262367b61de243d
clairedeboer <claire.deboer@gmail.com>
2021-01-14 23:40:24 -0500

    commit tree 2a669cfc1a226323cfd55463175a7efee3dc5080

--- src/index.js
+++ src/index.js
@@ -1,1 +1,30 @@
-console.log('%c HI', 'color: firebrick')
+console.log("%c HI", "color: firebrick");
+const dogImageContainer = document.querySelector("#dog-image-container");
+const dogBreedUl = document.querySelector("#dog-breeds");
+
+const renderImage = (photo) => {
+  const imageTag = document.createElement("img");
+  imageTag.src = photo;
+  dogImageContainer.append(imageTag);
+};
+
+const renderBreed = (breed) => {
+  const listItem = document.createElement("li");
+  listItem.innerHTML = breed;
+  dogBreedUl.append(listItem);
+  listItem.addEventListener("click", () => {
+    listItem.style.color = "blue";
+  });
+};
+
+fetch("https://dog.ceo/api/breeds/image/random/4")
+  .then((response) => response.json())
+  .then((photoData) =>
+    photoData.message.forEach((photo) => renderImage(photo))
+  );
+
+fetch("https://dog.ceo/api/breeds/list/all")
+  .then((response) => response.json())
+  .then((breedData) =>
+    Object.keys(breedData.message).forEach((breed) => renderBreed(breed))
+  );
